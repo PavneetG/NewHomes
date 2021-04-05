74c837fb74de692738bfb72ecd29ee197f122052
PavneetG <pavneetgill2@live.com>
2021-04-05 10:10:29 -0400

    Adding new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'; 
+import Navbar from "./components/Navbar/Navbar";
+import image1 from "./images/1.jpg"
+import image2 from "./images/2.jpg"
+import image3 from "./images/3.jpg"
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <AutoplaySlider
+      play={true}
+      cancelOnInteraction={true} // should stop playing on user interaction
+      interval={6000}
+      fillParent={false}
+      bullets={false}
+      >
+      <div data-src={image1}/>
+      <div data-src={image2}/>
+      <div data-src={image3}/>
+      </AutoplaySlider>
     </div>
   );
 }
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Communities',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Coming Soon',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }, 
+]
