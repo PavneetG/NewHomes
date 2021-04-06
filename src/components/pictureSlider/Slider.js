14c0d9cd27ac503f52e1d8de1de56502ff204553
PavneetG <pavneetgill2@live.com>
2021-04-06 15:55:25 -0400

    Adding slider css

--- src/components/pictureSlider/Slider.js
+++ src/components/pictureSlider/Slider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css'
+import './Slider.css'
 import image1 from "../../images/1.jpg"
 import image2 from "../../images/2.jpg"
 import image3 from "../../images/3.jpg"
