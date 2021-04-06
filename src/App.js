a4ab38d622f8a8ab1010b5afdca9b9ad5fc53160
PavneetG <pavneetgill2@live.com>
2021-04-05 23:28:30 -0400

    Changes for different pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import './App.css';
 import React from 'react'; 
 import Navbar from "./components/Navbar/Navbar";
-import image1 from "./images/1.jpg"
-import image2 from "./images/2.jpg"
-import image3 from "./images/3.jpg"
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/pages/Home'
 
-const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <AutoplaySlider
-      play={true}
-      cancelOnInteraction={true} // should stop playing on user interaction
-      interval={6000}
-      fillParent={false}
-      bullets={false}
-      >
-      <div data-src={image1}/>
-      <div data-src={image2}/>
-      <div data-src={image3}/>
-      </AutoplaySlider>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exect component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/components/pictureSlider/Slider.js
+++ src/components/pictureSlider/Slider.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import '../../App.css'
+import image1 from "../../images/1.jpg"
+import image2 from "../../images/2.jpg"
+import image3 from "../../images/3.jpg"
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+function Slider() {
+    return (
+      <div className="slider-container">
+        <AutoplaySlider
+        play={true}
+        cancelOnInteraction={true} // should stop playing on user interaction
+        interval={6000}
+        fillParent={false}
+        bullets={false}
+        >
+        <div data-src={image1}/>
+        <div data-src={image2}/>
+        <div data-src={image3}/>
+        </AutoplaySlider>
+      </div>
+    );
+  }
+
+export default Slider;
